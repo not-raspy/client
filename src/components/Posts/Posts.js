24904ad8e2b532ced90404f6ce38ce2ef8a2c3f8
not-raspy <54740570+not-raspy@users.noreply.github.com>
2021-04-10 03:31:53 +0200

    finished Form, fixed reducers and actions

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import Post from './Post/Post';
 import useStyles from './styles';
 
 const Posts = () => {
-    const posts = useSelector((state => state.posts));
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
     console.log(posts);
