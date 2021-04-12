8a6c650a38820e06f741702326ca17c1e858a551
not-raspy <54740570+not-raspy@users.noreply.github.com>
2021-04-12 02:07:17 +0200

    adding background, unfinished updating functionality

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,9 +1,11 @@
 export default (posts = [], action) => {
-    switch (action.type){
+    switch (action.type) {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
             return [...posts, action.payload];
+        case 'UPDATE':
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         default:
             return posts;
     }
